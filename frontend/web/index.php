9c7a4ecba7cbd2c3c846f3f118b8958484daf939
nerburish <nerburish@gmail.com>
2016-08-29 14:48:11 +0200

    Update index.php

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,7 +1,7 @@
 <?php
-segg
-echo "prueba3";
-exit;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Composer
 require(__DIR__ . '/../../vendor/autoload.php');
