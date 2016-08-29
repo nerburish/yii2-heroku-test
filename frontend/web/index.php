e2b87893413586b742ebad1e9680e3ddca526c44
nerburish <nerburish@gmail.com>
2016-08-29 18:54:50 +0200

    Update index.php

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -9,12 +9,8 @@ require(__DIR__ . '/../../vendor/autoload.php');
 // Environment
 require(__DIR__ . '/../../common/env.php');
 
-
-
 // Yii
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-
-
 
 // Bootstrap application
 require(__DIR__ . '/../../common/config/bootstrap.php');
